17af589f915e1bb1e8a7f3e68c0a628dd8354870
ravikumaraccionlabs <49903235+ravikumaraccionlabs@users.noreply.github.com>
2020-06-24 16:19:28 +0530

    Add files via upload

---
+++ src/main/java/com/falaApp/app/FalaApp1Application.java
@@ -0,0 +1,13 @@
+package com.falaApp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalaApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalaApp1Application.class, args);
+	}
+
+}
