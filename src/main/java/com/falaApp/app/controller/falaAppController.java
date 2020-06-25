8bdec3db41b63276c55a8586f033306cfc0063a4
Ravi <ravi.kumar@accionlabs.com>
2020-06-25 10:50:55 +0530

    added server port

--- src/main/java/com/falaApp/app/controller/falaAppController.java
+++ src/main/java/com/falaApp/app/controller/falaAppController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class falaAppController {
-	
+
 	@GetMapping(value = "/appName")
 	public String getName() {
 		return "My first app";
